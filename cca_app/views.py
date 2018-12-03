05291b3f142b16767f7b2f3347e535e9ed275c82
natalie-poulson <natalieplsn@gmail.com>
2018-12-03 15:13:54 -0800

    added error handling to views.py

--- cca_app/views.py
+++ cca_app/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from pathlib import Path
+from django.http import Http404
+
 
 def api (request):
-    # Open json file
-    json_file = open('cca_app/data/cca_mock_courses.json')
+    try:
+        file_path = Path('cca_app/data/cca_mock_courses.json')
 
-    # Deserialize to a Python Object
-    data = json.load(json_file)
+        json_file = open(file_path)
+        data = json.load(json_file)
 
-    # send a JSON-encoded response
+    except FileNotFoundError:
+        raise Http404("File does not exist")
+
     return JsonResponse(data)
+
+    # if file_path.exists():
+    #     json_file = open(file_path)
+    #     # Deserialize to a Python Object
+    #     data = json.load(json_file)
+    #     # send a JSON-encoded response
+    #     return JsonResponse(data)
+
+    # else:
+    #     return HttpResponse(status=500)
